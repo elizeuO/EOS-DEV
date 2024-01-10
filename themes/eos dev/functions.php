b2c9235d7cc65db09156f42193600a963f16bd9d
Elizeu <elizeu_os@hotmail.com>
2024-01-10 18:58:42 -0300

    Add header

---
+++ themes/eos dev/functions.php
@@ -0,0 +1,39 @@
+<?php 
+
+//removes wordpress admin bar
+add_filter('show_admin_bar', '__return_false');
+
+//Get theme image
+function getThemeImage($image)
+{
+    $path = '/dist/img/' . $image;
+    return get_template_directory_uri() . $path;
+}
+
+//Register and enqueue the scripts
+function registerScripts()
+{
+    $path = get_template_directory_uri() . '/dist/js/';
+    //Removes the default wordpress jquery script
+    wp_deregister_script('jquery');
+
+    wp_register_script('jquery', $path . 'jquery.min.js', array(), '3.7.1', true);
+    wp_enqueue_script('jquery');
+
+    wp_register_script('eos-dev', $path . 'eos-dev.js', array(), '1.0.0', true);
+    wp_enqueue_script('eos-dev');
+}
+
+add_action('wp_enqueue_scripts', 'registerScripts');
+
+//Register and enqueue the CSS styles
+function registerStyles()
+{
+    $path = get_template_directory_uri() . '/dist/style/';
+    wp_register_style('normalize', $path . 'normalize.min.css', array(), false, false);
+    wp_enqueue_style('normalize');
+
+    wp_register_style('eos-dev', $path . 'eos-dev.css', array(), false, false);
+    wp_enqueue_style('eos-dev');
+}
+add_action('wp_enqueue_scripts', 'registerStyles');
---
+++ themes/eos dev/partials/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+
+    <title>
+        EOS DEV | Sites e soluções digitais para pessoas e empresas
+    </title>
+
+</head>
+
+<?php wp_head(); ?>
+
+<body>
+    <header class="c-header">
+        <div class="c__container">
+            <div class="l-flex l-flex--center l-flex--wrap">
+
+                <a href="" class="c-header__logo l-flex__left">
+                    <img src="<?= getThemeImage('logo.webp')?>" alt="Eos dev">
+                </a>
+
+                <nav class="c-nav">
+                    <a href="/" class="c-nav__link">
+                        Início
+                    </a>
+                    <a href="#sobre-mim" class="c-nav__link">
+                        Sobre mim
+                    </a>
+                    <a href="#skills" class="c-nav__link">
+                        Skills
+                    </a>
+                   
+                    <a href="/#portfolio" class="c-nav__link">
+                        Portfólio
+                    </a>
+
+                    <a href="/#servicos" class="c-nav__link">
+                        Serviços
+                    </a>
+
+                    <a href="/#contato" class="c-nav__link">
+                        Fale comigo
+                    </a>
+                </nav>
+
+                <a href="/#contato" class="c__button l-flex__right">
+                    Vamos trabalhar juntos?
+                </a>
+
+            </div>
+        </div>
+
+    </header>
+</body>
+
+</html>
