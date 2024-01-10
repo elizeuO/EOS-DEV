37daf9e0aa7ac0f36703a0deb371d994a0a34729
Elizeu <elizeu_os@hotmail.com>
2024-01-10 20:08:26 -0300

    Add nav menu

--- themes/eos dev/partials/header.php
+++ themes/eos dev/partials/header.php
@@ -25,29 +25,7 @@
                     <img src="<?= getThemeImage('logo.webp')?>" alt="Eos dev">
                 </a>
 
-                <nav class="c-nav">
-                    <a href="/" class="c-nav__link">
-                        Início
-                    </a>
-                    <a href="#sobre-mim" class="c-nav__link">
-                        Sobre mim
-                    </a>
-                    <a href="#skills" class="c-nav__link">
-                        Skills
-                    </a>
-                   
-                    <a href="/#portfolio" class="c-nav__link">
-                        Portfólio
-                    </a>
-
-                    <a href="/#servicos" class="c-nav__link">
-                        Serviços
-                    </a>
-
-                    <a href="/#contato" class="c-nav__link">
-                        Fale comigo
-                    </a>
-                </nav>
+              <?php include('menu.php'); ?>
 
                 <a href="/#contato" class="c__button l-flex__right">
                     Vamos trabalhar juntos?
---
+++ themes/eos dev/partials/menu.php
@@ -0,0 +1,23 @@
+<nav class="c-nav">
+  <a href="/" class="c-nav__link">
+      Início
+  </a>
+  <a href="#sobre-mim" class="c-nav__link">
+      Sobre mim
+  </a>
+  <a href="#skills" class="c-nav__link">
+      Skills
+  </a>
+ 
+  <a href="/#portfolio" class="c-nav__link">
+      Portfólio
+  </a>
+
+  <a href="/#servicos" class="c-nav__link">
+      Serviços
+  </a>
+
+  <a href="/#contato" class="c-nav__link">
+      Fale comigo
+  </a>
+</nav>
