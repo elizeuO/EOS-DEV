17c51cf9caaf94a41e8ddc41d9cbd597b44b143b
Elizeu <elizeu_os@hotmail.com>
2024-01-10 21:11:49 -0300

    Add hero section

--- themes/eos-dev/index.php
+++ themes/eos-dev/index.php
@@ -1,4 +1,5 @@
 <?php 
 include('partials/header.php');
+include('sections/inicio.php');
 include('partials/footer.php');
 ?>
---
+++ themes/eos-dev/sections/inicio.php
@@ -0,0 +1,26 @@
+<section class="c-section c-section--hero" id="inicio">
+    <div class="c__container">
+
+        <div class="c-section--hero__content c__center">
+
+            <h1>
+                <b>
+                    O seu negócio
+                </b>
+                sob um novo olhar no
+                <b>
+                    mundo digital
+                </b>
+
+                <span class="c-section--hero__typed-text">
+                    Desenvolvo soluções
+                </span>
+            </h1>
+
+        </div>
+
+        <a href="#sobre-mim" class="c-section--hero__mouse-icon">
+            <img src="<?= getThemeImage('mouse.webp') ?>">
+        </a>
+    </div>
+</section>
