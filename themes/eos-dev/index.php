bd21e00fe4a7943fb492c2f5fecb4e2bfddeff1e
Elizeu <elizeu_os@hotmail.com>
2024-01-11 16:00:05 -0300

    Add section counter

--- themes/eos-dev/index.php
+++ themes/eos-dev/index.php
@@ -2,5 +2,6 @@
 include('partials/header.php');
 include('sections/inicio.php');
 include('sections/sobre-mim.php');
+include('sections/counter.php');
 include('partials/footer.php');
 ?>
---
+++ themes/eos-dev/sections/counter.php
@@ -0,0 +1,81 @@
+<section class="c-section c-section--less-padding">
+    <div class="c__container">
+        <div class="l-flex l-flex--center l-flex--negative l-flex--wrap l-flex--stretch">
+
+            <article class="l__col-3">
+
+                <div class="c-counter">
+                    <div class="l-flex l-flex--center l-flex--wrap">
+                        <img src="<?= getThemeImage('counter-icon-experience.webp') ?>" alt="Anos de experiência"
+                            class="c-counter__img">
+
+                        <div class="c-counter__info">
+                            <div class="c-counter__number js-counter">
+                                5
+                            </div>
+                            Anos de experiência
+                        </div>
+                    </div>
+                </div>
+
+            </article>
+
+            <article class="l__col-3">
+
+                <div class="c-counter">
+                    <div class="l-flex l-flex--center l-flex--wrap">
+
+                        <img src="<?= getThemeImage('counter-icon-project.webp') ?>" alt="Projetos desenvovidos"
+                            class="c-counter__img">
+
+                        <div class="c-counter__info">
+                            <div class="c-counter__number js-counter">
+                                50
+                            </div>
+                            Projetos desenvovidos
+                        </div>
+                    </div>
+                </div>
+
+            </article>
+
+
+
+            <article class="l__col-3">
+
+                <div class="c-counter">
+                    <div class="l-flex l-flex--center l-flex--wrap">
+                        <img src="<?= getThemeImage('counter-icon-customer.webp') ?>" alt="Clientes satisfeitos"
+                            class="c-counter__img">
+
+                        <div class="c-counter__info">
+                            <div class="c-counter__number js-counter">
+                                20
+                            </div>
+                            Clientes satisfeitos
+                        </div>
+                    </div>
+                </div>
+
+            </article>
+
+            <article class="l__col-3">
+
+                <div class="c-counter">
+                    <div class="l-flex l-flex--center l-flex--wrap">
+                        <img src="<?= getThemeImage('counter-icon-coffee.webp') ?>" alt="Café" class="c-counter__img">
+
+                        <div class="c-counter__info">
+                            <div class="c-counter__number js-counter">
+                                1000
+                            </div>
+                            Litros de café ingeridos
+                        </div>
+                    </div>
+                </div>
+
+            </article>
+
+        </div>
+    </div>
+</section>
